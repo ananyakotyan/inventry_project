c5ced939856f6f7083cbd9749005a27c1a7b640c
ananyakotyan <anuananya626@gmail.com>
2025-02-11 12:39:59 +0530

    goods

--- goods.java
+++ goods.java
@@ -1,5 +1,7 @@
+
 public class Welcome {
     public static void main(String[] args) {
         System.out.println("This program is for GOODS purchase in Module");
+        system.out.println("This code have been updated in vs");
     }
 }
